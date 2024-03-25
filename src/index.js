0ad8199b9af00f112008446e7624d9f41f8d7f51
brancrasto <quantamcrasto@gmail.com>
2024-03-25 19:49:34 -0400

    spaces to tabs

--- src/index.js
+++ src/index.js
@@ -5,51 +5,51 @@ import { oneDark } from "@codemirror/theme-one-dark";
 import { vim } from "@replit/codemirror-vim"
 
 const vimChallenges = [
-  [
-    {
-        "id": 1,
-        "buggyCode": "int main() {\n    printf('Hello, world!');\n}",
-        "description": "The printf statement uses incorrect string delimiters.",
-        "fixedCode": "int main() {\n    printf(\"Hello, world!\");\n}"
-    }
-  ]
+	[
+		{
+			"id": 2,
+			"buggyCode": "int main() {\n    printf('Hello, world!');\n}",
+			"description": "The printf statement uses incorrect string delimiters.",
+			"fixedCode": "int main() {\n    printf(\"Hello, world!\");\n}"
+		}
+	]
 ];
 
 const state = EditorState.create({
-  extensions: [basicSetup, oneDark, vim()],
+	extensions: [basicSetup, oneDark, vim()],
 });
 
 const editor = new EditorView({
-  state,
-  parent: document.getElementById('editor')
+	state,
+	parent: document.getElementById('editor')
 });
 
 function loadChallenge(challenge) {
-  document.querySelector(".description").innerHTML = challenge.description;
-  editor.setState(EditorState.create({
-    doc:challenge.buggyCode,
-    extensions: [basicSetup, oneDark, vim()],
-  }));  
+	document.querySelector(".description").innerHTML = challenge.description;
+	editor.setState(EditorState.create({
+		doc:challenge.buggyCode,
+		extensions: [basicSetup, oneDark, vim()],
+	}));
 }
 
 function checkSolution(challenge) {
-  const userCode = editor.state.doc.toString();
+	const userCode = editor.state.doc.toString();
 
-  if (userCode === challenge.fixedCode) {
-    console.log("Correct");
-    document.querySelector(".description").innerHTML = "Correct";
-  } else {
-    console.log("Fail");
-    document.querySelector(".description").innerHTML = "Fail";
-  }
+	if (userCode === challenge.fixedCode) {
+		console.log("Correct");
+		document.querySelector(".description").innerHTML = "Correct";
+	} else {
+		console.log("Fail");
+		document.querySelector(".description").innerHTML = "Fail";
+	}
 }
 
 vimChallenges.forEach((challengeGroup) => {
-  challengeGroup.forEach((challenge) => {
-    loadChallenge(challenge);
+	challengeGroup.forEach((challenge) => {
+		loadChallenge(challenge);
 
-    document.getElementById('checkBtn').addEventListener('click', () => {
-      checkSolution(challenge);
-    });
-  })
+		document.getElementById('checkBtn').addEventListener('click', () => {
+			checkSolution(challenge);
+		});
+	})
 })
